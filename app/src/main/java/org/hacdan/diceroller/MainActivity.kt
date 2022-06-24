b2a874effc4f4b2593ee84e965bd687c63ad46d8
HacDan <david.mcclellan.p@gmail.com>
2022-06-23 20:22:52 -0400

    Switch to using images instead of a text view. Refactored dice roll to a private function

--- app/src/main/java/org/hacdan/diceroller/MainActivity.kt
+++ app/src/main/java/org/hacdan/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package org.hacdan.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,19 +11,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        rollDice()
+
         val rollButton: Button = findViewById(R.id.btnRoll)
-        val diceValueTV: TextView = findViewById(R.id.tvDiceRoll)
-        val diceSixSides = Dice(6)
-
-        diceValueTV.text = diceSixSides.roll().toString()
-
         rollButton.setOnClickListener{
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
-            diceValueTV.text = diceSixSides.roll().toString()
+            rollDice()
         }
 
 
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceIV: ImageView = findViewById(R.id.ivDice)
+
+        val drawableResID = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceIV.setImageResource(drawableResID)
+        diceIV.contentDescription = diceRoll.toString()
     }
 }
 
@@ -31,5 +47,4 @@ class Dice (private val numSides : Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
-
 }
